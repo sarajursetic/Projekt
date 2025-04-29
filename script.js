616e657e3cfebcf9a42e8cc1ca18c3f7fa320b65
Sara Juršetić <sara.jursetic@gmail.com>
2025-04-29 08:18:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function sidebar() {
+  const sidebar = document.querySelector(".sidebar-menu");
+  sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar-menu");
+  sidebar.style.display = "none";
+}
+
+
